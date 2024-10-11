8f79fc1cf75287c39c549e9b5986520e36aefbc3
Shreya-ashoka <ashokashreya34@gmail.com>
2024-10-11 19:26:24 +0530

    has build devo

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint:{
-    ignoreDuringBuilds: true,
-  }
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
